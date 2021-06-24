f44f6c2863cc030218879861cdef244c76e9489d
onkarsherkar <onkar.sherkar@gmail.com>
2021-06-24 14:42:43 +0530

    models added

--- Library/books/models.py
+++ Library/books/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    issued_book = models.IntegerField(default=0, validators=[
+        MinValueValidator(0), MaxValueValidator(2)
+    ])
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    genre = models.ManyToManyField(Genre)
+    is_issued = models.BooleanField(default=False)
+
+class Book_issue(models.Model):
+    book = models.ForeignKey(Books,on_delete=models.CASCADE)
+    date_issue = models.DateTimeField()
+    date_return = models.DateTimeField()
+    user = models.ForeignKey(CustomUser,on_delete=CASCADE)
+
+class Book_return(models.Model):
+    book = models.ForeignKey(Books,on_delete=models.CASCADE)
+    date_return = models.DateTimeField()
+    user = models.ForeignKey(CustomUser,on_delete=CASCADE)
+    charge = models.IntegerField()
+    
--- Library/books/views.py
+++ Library/books/views.py
@@ -1,3 +1,7 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
-
+from django.http.response import JsonResponse
 # Create your views here.
+
+def test(request):
+    return JsonResponse({'test':'OK'})
